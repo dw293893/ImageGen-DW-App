4eefdd3356a3b2380bed9b39824f4e143ab86616
Daniel <dw293893@gmail.com>
2024-07-24 15:26:29 +0100

    fixed data types in clerk webhook

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -66,7 +66,7 @@ export async function POST(req: Request) {
       email: email_addresses[0].email_address,
       username: username!,
       firstName: first_name ?? "",
-      lastName: last_name "",
+      lastName: last_name ?? "",
       photo: image_url,
     };
 
@@ -89,9 +89,9 @@ export async function POST(req: Request) {
     const { id, image_url, first_name, last_name, username } = evt.data;
 
     const user = {
-      firstName: first_name,
-      lastName: last_name,
-      username: username!,
+      firstName: first_name ?? "",
+      lastName: last_name ?? "",
+      username: username! ?? "",
       photo: image_url,
     };
 
